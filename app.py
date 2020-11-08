1a7deec23bbb180caf3b6393583179529d94d2ea
MadBad3 <b.hamdi3@hotmail.fr>
2020-11-08 22:43:43 +0100

    final application

--- app.py
+++ app.py
@@ -1,54 +1,23 @@
-from flask import Flask, request, render_template
-from redis import Redis, RedisError, StrictRedis
-import json
+from flask import Flask, request, jsonify, render_template
+import pickle
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
 app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
 
-def add_sentence(sentID, the_sentence):
-	sentence = {'sentence_id': sentID,
-			'sentence': the_sentence
-			}
-	status = "success"
-	try:
-		redis_client.set(sentID, json.dumps(sentence))
-	except RedisError:
-		status = "fail"
-	
-	return status
-	
-	
-def view_sentence(sentID):
-	sentence = ''
-	status = ""
-	try:
-		sentence = redis_client.get(sentID)
-	except RedisError:
-		status = "fail"
-		
-	if status is not 'fail' and sentence is not '':
-		sentence = json.loads(sentence)
-		status = "sentence ID: {}, the sentence: {}".format(
-								sentence['sentence_id'],
-								sentence['sentence'],
-								)
-	return status
-	
-@app.route('/', methods=['GET', 'POST'])
-def index():
-	if request.method == 'POST':
-		details = request.form
-		if details['form_type'] == 'add_sentence':
-			return add_sentence(details['sentID'], details['the_sentence'])
-			
-		elif details['form_type'] == 'view_sentence':
-			return view_sentence(details['sentID'])
-	return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-	redis_client = StrictRedis(host='redis', port=6379)
-	app.run(host='0.0.0.0')
-	
-	
-	
-	
-	
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = request.form.values()
+    sentence = pd.Series(int_features)
+    predicted = model.predict(sentence)
+
+    return render_template('index.html', prediction_text='The sentiment is: {}'.format(predicted))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
