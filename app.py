405988dc0dacdb4fc9022d45912925bc5d84cdf1
MadBad3 <b.hamdi3@hotmail.fr>
2020-11-09 22:52:23 +0100

    perfect one

--- app.py
+++ app.py
@@ -7,18 +7,19 @@ import pandas as pd
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
+    if request.method == 'POST':
+    	details = request.form
+    	if details['form_type'] == 'view_sentence':
+    		int_features = request.form.values()
+    		sentence = pd.Series(int_features)
+    		predicted = model.predict(sentence)
+    		prediction_text='The sentiment of this sentence is {}'.format(predicted[0])
+    		return prediction_text
+
     return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-
-    int_features = request.form.values()
-    sentence = pd.Series(int_features)
-    predicted = model.predict(sentence)
-
-    return render_template('index.html', prediction_text='The sentiment of this sentence is {}'.format(predicted[0]))
+    
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
