c9b2cc1063e4ed266a6094ecfe6f2dff4419841f
MadBad3 <b.hamdi3@hotmail.fr>
2020-11-09 22:52:36 +0100

    perfect one

--- test_app.py
+++ test_app.py
@@ -13,7 +13,15 @@ class FlaskTests(unittest.TestCase):
 	def test_a_index(self):
 		response = requests.get('http://localhost:5000')
 		self.assertEqual(response.status_code, 200)
-	
+
+	def test_b_sentiment_result(self):	
+		params = {
+			'uid': 'I hate this show !',
+			'form_type': "view_sentence"
+		}
+		response = requests.post('http://localhost:5000', data=params)
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.content, b'The sentiment of this sentence is negative')
 
 if __name__ == '__main__':
 	unittest.main()
