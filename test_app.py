8b7a300f5ead73fc872322b3ca545a8571aaba4f
MadBad3 <b.hamdi3@hotmail.fr>
2020-11-10 11:53:08 +0100

    final version

--- test_app.py
+++ test_app.py
@@ -16,12 +16,29 @@ class FlaskTests(unittest.TestCase):
 
 	def test_b_sentiment_result(self):	
 		params = {
-			'uid': 'I hate this show !',
+			'sentence': 'I hate this show !',
 			'form_type': "view_sentence"
 		}
 		response = requests.post('http://localhost:5000', data=params)
 		self.assertEqual(response.status_code, 200)
 		self.assertEqual(response.content, b'The sentiment of this sentence is negative')
 
+	def test_c_sentiment_result(self):	
+		params = {
+			'sentence': 'I like the movie !',
+			'form_type': "view_sentence"
+		}
+		response = requests.post('http://localhost:5000', data=params)
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.content, b'The sentiment of this sentence is positive')
+
+	def test_d_sentiment_result(self):	
+		params = {
+			'sentence': 'He is fine but not today',
+			'form_type': "view_sentence"
+		}
+		response = requests.post('http://localhost:5000', data=params)
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.content, b'The sentiment of this sentence is neutral')
 if __name__ == '__main__':
 	unittest.main()
